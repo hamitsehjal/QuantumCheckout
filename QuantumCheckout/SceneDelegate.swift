379a66c29d64bd5903d7371dd6350669e68895d6
Hamit Sehjal <hamitsehjal26@gmail.com>
2024-03-02 19:42:42 -0500

    Fixed switching between views in Tab Bar

--- QuantumCheckout/SceneDelegate.swift
+++ QuantumCheckout/SceneDelegate.swift
@@ -22,7 +22,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let itemStore=ItemStore()
         
         // Access the ItemsViewController and set its item store
-        let navController=window!.rootViewController as! UINavigationController
+        let tabController=window!.rootViewController as! UITabBarController
+        let navController=tabController.viewControllers?[0] as! UINavigationController
         let itemsController=navController.topViewController as! ItemsViewController
 //        let itemsController = window!.rootViewController as! ItemsViewController
         itemsController.itemStore=itemStore
